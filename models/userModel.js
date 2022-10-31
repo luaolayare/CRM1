7f2b5603b7f1ecfd594544a44bf32c3fe2217790
Luis Alfredo LO. Olaya Requenet <lolaya@AXIOMA.LOCAL>
2022-10-30 20:28:32 -0500

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    telefono: {
+      type: String,
+      required: [true, "Please add an telefono"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
